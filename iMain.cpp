2a059afc1d281de641efc6795fae530df15c3113
MdSium003 <mdsium2004@gmail.com>
2024-02-05 21:46:35 +0600

    Added most latest working code

--- iMain.cpp
+++ iMain.cpp
@@ -8,6 +8,7 @@ What is left;
 */
 
 char turn = 'W';
+int page = 0;
 int board[10][10];
 int temp_board[10][10];
 int moveable[64][2];
@@ -19,8 +20,10 @@ int temp_eliminate_index = 0;
 int count = 0;
 int selectedX,selectedY;
 bool selected = false;
+bool castling[4] = {true, true, true, true};
 
 int white_x, white_y, black_x, black_y;
+int temp_white_x, temp_white_y, temp_black_x, temp_black_y;
 
 char path[30][50] = {
 	"Pic\\\\Pieces\\\\white-king.bmp",
@@ -49,6 +52,16 @@ char path[30][50] = {
 	"Pic\\\\Red_marked\\\\black-pawn.bmp"
 };
 
+char BoardPic[30][50] = {
+	"Pic\\\\ChessBoard\\\\Chess1.bmp",
+	"Pic\\\\ChessBoard\\\\Chess2.bmp",
+	"Pic\\\\ChessBoard\\\\Chess3.bmp",
+	"Pic\\\\ChessBoard\\\\Chess4.bmp",
+	"Pic\\\\ChessBoard\\\\Chess5.bmp",
+	"Pic\\\\ChessBoard\\\\Chess6.bmp"
+};
+int Board_index = 0;
+
 struct Piece
 {
 	char name;
@@ -71,6 +84,10 @@ struct Piece
 Piece types[7];
 
 void init(){
+	turn = 'W';
+	moveable_index = 0;
+	eliminate_index = 0;
+	temp_eliminate_index = 0;
 	int arr[8] = {1,-1,-1,1,1,-1,0,0};
 	int arr2[8] = {1,-1,1,-1,0,0,1,-1};
 	types[1] = Piece('K',1,8,arr,arr2,1);
@@ -116,7 +133,10 @@ void init(){
 	white_x = 5, white_y = 1, black_x = 5, black_y = 8;
 }
 
+void GetCoordinates(int x, int y);
+
 void copy_board(){
+	temp_white_x = white_x, temp_white_y = white_y, temp_black_x = black_x, temp_black_y = black_y;
 	for(int i=0;i<10;i++){
 		for(int j=0;j<10;j++){
 			temp_board[i][j] = board[i][j];
@@ -124,59 +144,87 @@ void copy_board(){
 	}
 }
 
+bool win_lose(){
+	for(int i=1;i<=8;i++){
+		for(int j=1;j<=8;j++){
+			if(turn == 'B' && board[j][i] <0){
+				GetCoordinates(i,j);
+				if(moveable_index != 0 || eliminate_index != 0) {
+					moveable_index = 0;
+					eliminate_index = 0;
+					return false;
+				}
+			} 
+			else if(turn == 'W' && board[j][i]>0){
+				GetCoordinates(i,j);
+				if(moveable_index != 0 || eliminate_index != 0) {
+					moveable_index = 0;
+					eliminate_index = 0;
+					return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+
 void temp_eliminating(int x, int y){
 	temp_eliminate_index = 0;
-	int p = (board[y][x]>0)?board[y][x]:-1*board[y][x];
+	int p = (temp_board[y][x]>0)?temp_board[y][x]:-1*temp_board[y][x];
+	//printf("%d\n", p);
 	for(int i=0;i<types[p].dirsize;i++){
 		int movex = x;
 		int movey = y;
-		printf("%d %d\n",movex,movey);
+		//printf("%d %d\n",movex,movey);
 		for(int j=0;j<types[p].moveCount;j++){
 			movex = movex + types[p].dirX[i];
-			if(board[y][x] == -6) movey = movey - types[p].dirY[i];
+			if(temp_board[y][x] == -6) movey = movey - types[p].dirY[i];
 			else movey = movey + types[p].dirY[i];
-
-			if(movex <= 0 || movey <= 0 || movex >= 10 || movey >= 10) continue;
-			else if(board[movey][movex] == 99) break;
-
-			if((board[y][x] == 6 || board[y][x] == -6) && j == 0){
-				if((movex+1)<9 && board[movey][movex+1]*board[y][x] < 0 ){
-					temp_eliminate[eliminate_index][0] = movex+1;
-					temp_eliminate[eliminate_index][1] = movey;
-					printf("eli - %d %d\n", movex, movey);
+			
+			if(movex <0 || movey <0 || movex >9 || movey> 9) continue;
+			else if(temp_board[movey][movex] == 99) break;
+			if((temp_board[y][x] == 6 || temp_board[y][x] == -6) && j == 0){
+				if((movex+1)<9 && temp_board[movey][movex+1]*temp_board[y][x] < 0 ){
+					temp_eliminate[temp_eliminate_index][0] = movex+1;
+					temp_eliminate[temp_eliminate_index][1] = movey;
+					//printf("eli - %d %d\n", movex, movey);
 					temp_eliminate_index++;
 				}
-				if((movex-1)>0 && board[movey][movex-1]*board[y][x] < 0){
-					temp_eliminate[eliminate_index][0] = movex-1;
-					temp_eliminate[eliminate_index][1] = movey;
-					printf("eli - %d %d\n", movex, movey);
+				if((movex-1)>0 && temp_board[movey][movex-1]*temp_board[y][x] < 0){
+					temp_eliminate[temp_eliminate_index][0] = movex-1;
+					temp_eliminate[temp_eliminate_index][1] = movey;
+					//printf("eli - %d %d\n", movex, movey);
 					temp_eliminate_index++;
 				}
 			}
 
-			if(board[movey][movex]*board[y][x] < 0 && board[y][x] != 6 && board[y][x] != -6) {
-				temp_eliminate[eliminate_index][0] = movex;
-				temp_eliminate[eliminate_index][1] = movey;
+			if(temp_board[movey][movex] == 0){}
+			else if(temp_board[movey][movex]*temp_board[y][x] < 0 && temp_board[y][x] != 6 && temp_board[y][x] != -6) {
+				temp_eliminate[temp_eliminate_index][0] = movex;
+				temp_eliminate[temp_eliminate_index][1] = movey;
 				printf("eli - %d %d\n", movex, movey);
 				temp_eliminate_index++;
 				break;
 			}
 			else break;
 			if(board[y][x] == 6 && y != 2)break;
-			if(board[y][x] == -6 && y != 7)break;
+			else if(board[y][x] == -6 && y != 7)break;
 		}
 	}
 }
 
 bool checksystem(int x, int y){
+	printf("Position of King = %d - %d\n", y, x);
 	for(int i=1;i<=8;i++){
 		for(int j=1;j<=8;j++){
 			if(temp_board[j][i]*temp_board[y][x] < 0){
 				temp_eliminate_index = 0;
-				temp_eliminating(j,i);
+				temp_eliminating(i,j);
 				for(int k=0;k<temp_eliminate_index;k++){
+					printf("Elimimate to %d = %d - %d\n", temp_board[j][i], temp_eliminate[k][0] , temp_eliminate[k][1]);
 					if(temp_eliminate[k][0] == x && temp_eliminate[k][1] == y) return true;
 				}
+				printf("-----------------------------\n");
 			}
 		}
 	}
@@ -206,6 +254,7 @@ void GetCoordinates(int x, int y){
 					printf("eli - %d %d\n", movex, movey);
 					eliminate_index++;
 
+					copy_board();
 					temp_board[movey][movex+1] = temp_board[y][x];
 					temp_board[y][x] = 0;
 					bool check = (turn == 'W')?checksystem(white_x, white_y):checksystem(black_x, black_y);
@@ -217,11 +266,11 @@ void GetCoordinates(int x, int y){
 					printf("eli - %d %d\n", movex, movey);
 					eliminate_index++;
 
+					copy_board();
 					temp_board[movey][movex-1] = temp_board[y][x];
 					temp_board[y][x] = 0;
 					bool check = (turn == 'W')?checksystem(white_x, white_y):checksystem(black_x, black_y);
 					if(check) eliminate_index--;
-					copy_board();
 				}
 			}
 
@@ -231,36 +280,39 @@ void GetCoordinates(int x, int y){
 				moveable[moveable_index][1] = movey;
 				moveable_index++;
 
+				copy_board();
 				temp_board[movey][movex] = temp_board[y][x];
 				temp_board[y][x] = 0;
-				bool check = (turn == 'W')?checksystem(white_x, white_y):checksystem(black_x, black_y);
+				if(temp_board[movey][movex] == 1)  temp_white_x = movex,temp_white_y = movey;
+				else if(temp_board[movey][movex] == -1)  temp_black_x = movex,temp_black_y = movey;
+				bool check = (turn == 'W')?checksystem(temp_white_x, temp_white_y):checksystem(temp_black_x, temp_black_y);
 				if(check) moveable_index--;
-				copy_board();
 			}
-			
 			else if(board[movey][movex]*board[y][x] < 0 && board[y][x] != 6 && board[y][x] != -6) {
 				eliminate[eliminate_index][0] = movex;
 				eliminate[eliminate_index][1] = movey;
 				printf("eli - %d %d\n", movex, movey);
 				eliminate_index++;
 
+				copy_board();
 				temp_board[movey][movex] = temp_board[y][x];
 				temp_board[y][x] = 0;
-				bool check = (turn == 'W')?checksystem(white_x, white_y):checksystem(black_x, black_y);
+				if(temp_board[movey][movex] == 1)  temp_white_x = movex,temp_white_y = movey;
+				else if(temp_board[movey][movex] == -1)  temp_black_x = movex,temp_black_y = movey;
+				bool check = (turn == 'W')?checksystem(temp_white_x,temp_white_y):checksystem(temp_black_x, temp_black_y);
 				if(check) eliminate_index--;
-				copy_board();
 				break;
 			}
 			else break;
 			if(board[y][x] == 6 && y != 2)break;
-			if(board[y][x] == -6 && y != 7)break;
+			else if(board[y][x] == -6 && y != 7)break;
 		}
 	}
 }
 
 void iDraw() {
 	iClear();
-	iShowBMP(0, 0, "Pic\\Chess.bmp");
+	iShowBMP(0, 0, BoardPic[Board_index]);
 	int x = (52+4), y = (52+4);
 	int i,j;
 	for(i=1;i<=8;i++){
@@ -294,7 +346,20 @@ void iDraw() {
 			iShowBMP2(cirx,ciry,path[(((p*-1)-1)*4)+3],0);
 		}
 	}
-		
+	if(page == 0){
+		iShowBMP(0,0,"Pic\\page1.bmp");
+		iSetColor(0,0,0);
+		iText(250, 150, "Press space to Enter", GLUT_BITMAP_TIMES_ROMAN_24);
+	}
+	if(page == 3) {
+		if(turn == 'W') {
+			iShowBMP(150,100,"Pic\\page2_black.bmp");
+		}
+		else if(turn == 'B') {
+			iShowBMP(150,100,"Pic\\page2_white.bmp");
+		}
+	}
+	if(page == 4) iText(100,100, "Keu jite gese", GLUT_BITMAP_TIMES_ROMAN_24);	
 }
 
 /*
@@ -302,7 +367,7 @@ void iDraw() {
 	(mx, my) is the position where the mouse pointer is.
 	*/
 void iMouseMove(int mx, int my) {
-	printf("x = %d, y= %d\n",mx,my);
+	//printf("x = %d, y= %d\n",mx,my);
 }
 
 /*
@@ -312,7 +377,34 @@ void iMouseMove(int mx, int my) {
 void iMouse(int button, int state, int mx, int my) {
 	int movex = (mx>=52 && mx<=652)?((mx-52)/75)+1:0;
 	int movey = (my>=52 && my<=652)?((my-52)/75)+1:0;
-	printf("%d %d\n", movex, movey);
+	//printf("%d %d\n", movex, movey);
+	if(page == 0) return;
+	else if(page == 3) {
+		mx -= 150;
+		my -= 100;
+		if(mx>7 && mx<200){
+			if(my>12 && my<=203){
+				board[selectedY][selectedX] = (board[selectedY][selectedX]>0)?2:-2;
+				page = 2;
+			}
+			else if(my>203 && my<395){
+				board[selectedY][selectedX] = (board[selectedY][selectedX]>0)?3:-3;
+				page = 2;
+			}
+		}
+		else if(mx>=200 && mx<393){
+			if(my>12 && my<=203){
+				board[selectedY][selectedX] = (board[selectedY][selectedX]>0)?5:-5;
+				page = 2;
+			}
+			else if(my>203 && my<395){
+				board[selectedY][selectedX] = (board[selectedY][selectedX]>0)?4:-4;
+				page = 2;
+			}
+		}
+		return;
+	}
+
 	count++;
 	if (button == GLUT_LEFT_BUTTON && movex<9 && movex>0 && movey<9 && movey>0) {
 		
@@ -342,6 +434,16 @@ void iMouse(int button, int state, int mx, int my) {
 				board[movey][movex] = board[selectedY][selectedX];
 				board[selectedY][selectedX] = 0;
 				turn = (turn == 'W')?'B':'W';
+				if(board[movey][movex] == 6 && movey == 8) {
+					page = 3;
+					selectedX = movex;
+					selectedY = movey;
+				}
+				else if(board[movey][movex] == -6 && movey == 1) {
+					page = 3;
+					selectedX = movex;
+					selectedY = movey;
+				}
 			}
 			if(board[movey][movex] == 1) {
 				white_x = movex;
@@ -353,7 +455,7 @@ void iMouse(int button, int state, int mx, int my) {
 			}
 			moveable_index = 0;
 			eliminate_index = 0;
-			copy_board();
+			if(win_lose()) page = 4;
 		}
 	}
 }
@@ -365,6 +467,11 @@ void iMouse(int button, int state, int mx, int my) {
 void iKeyboard(unsigned char key) {
 	if (key == ' ') {
 		init();
+		page = 2;
+	}
+	if(key == 'b'){
+		Board_index++;
+		if(Board_index >= 6) Board_index = 0;
 	}
 	//place your codes for other keys here
 }
