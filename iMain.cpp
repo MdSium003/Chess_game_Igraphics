4e6233fb9dbed90ddba54d9982e7ed04c98f3a38
MdSium003 <mdsium2004@gmail.com>
2024-02-07 07:57:05 +0600

    Removed Unused Comment

--- iMain.cpp
+++ iMain.cpp
@@ -1,12 +1,5 @@
 # include "iGraphics.h"
 
-/*
-What is left;
-	1. starting page;
-	2. check system;
-	3. castling king and rook;
-*/
-
 char turn = 'W';
 int page = 0;
 int board[10][10];
@@ -510,18 +503,10 @@ void iDraw() {
 	}
 }
 
-/*
-	function iMouseMove() is called when the user presses and drags the mouse.
-	(mx, my) is the position where the mouse pointer is.
-	*/
 void iMouseMove(int mx, int my) {
 	//printf("x = %d, y= %d\n",mx,my);
 }
 
-/*
-	function iMouse() is called when the user presses/releases the mouse.
-	(mx, my) is the position where the mouse pointer is.
-	*/
 void iMouse(int button, int state, int mx, int my) {
 	int movex = (mx>=52 && mx<=652)?((mx-52)/75)+1:0;
 	int movey = (my>=52 && my<=652)?((my-52)/75)+1:0;
@@ -629,10 +614,6 @@ void iMouse(int button, int state, int mx, int my) {
 	}
 }
 
-/*
-	function iKeyboard() is called whenever the user hits a key in keyboard.
-	key- holds the ASCII value of the key pressed.
-	*/
 void iKeyboard(unsigned char key) {
 	if (key == ' ') {
 		init();
@@ -645,15 +626,6 @@ void iKeyboard(unsigned char key) {
 	//place your codes for other keys here
 }
 
-/*
-	function iSpecialKeyboard() is called whenver user hits special keys like-
-	function keys, home, end, pg up, pg down, arraows etc. you have to use
-	appropriate constants to detect them. A list is:
-	GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
-	GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12,
-	GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP,
-	GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT
-	*/
 void iSpecialKeyboard(unsigned char key) {
 
 	if (key == GLUT_KEY_END) {
